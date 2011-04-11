3c9d55fe4cc7ab2ff3965691ff0f33452780a80b
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2011-04-11 21:28:21 +0000

    Updating documentation, license

--- DataURLMaker/STUtil.h
+++ DataURLMaker/STUtil.h
@@ -1,6 +1,6 @@
 /*
     DataURLMaker
-    Copyright (C) 2006-2008 Sveinbjorn Thordarson <sveinbjornt@simnet.is>
+    Copyright (C) 2006-2008 Sveinbjorn Thordarson <sveinbjornt@gmail.com>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
--- dataurlmaker.pl
+++ dataurlmaker.pl
@@ -1,31 +1,58 @@
 #!/usr/bin/perl -w
 #
-# (c) 2006 Sveinbjorn Thordarson <sveinbjornt@simnet.is>
+# (c) 2006-2011 Sveinbjorn Thordarson <sveinbjornt@simnet.is>
 #
-# This Perl command line program accepts file paths to images as arguments
-# and dumps out an RFC-compliant Data URL HTML IMG tag for each image.
+# This Perl command line program accepts files as arguments
+# and dumps out an RFC-compliant Data URL for each file.
 #
-# The Image::Info and MIME::Base64 modules are required.  To install these
-# modules, do the following:
+# The -i option generates an HTML img tag for the Data URL
 #
-# perl -MCPAN -e shell
-#
-# install Image::Info
-# install MIME:Base64
-#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
 
 use strict;
 use Image::Info qw(image_info dim);
 use MIME::Base64;
 
+our $VERSION = 2.0;
+my $scriptname = 'dataurlmaker.pl';
+my $valid_opts = 'i'
+
+# Getopt
+$Getopt::Std::STANDARD_HELP_VERSION = 1; # so --help quits after usage string
+
+sub main::HELP_MESSAGE
+{
+    print STDERR <<"EOF";
+$scriptname
+
+    Usage: $scriptname [-$valid_opts] file1 file2 ..."
+    
+        -i Generate HTML img tag for an image data URL
+        
+EOF
+}
+
 # check num arguments
 if (!scalar(@ARGV))
 {
-	print "Usage:\n";
-	print "\t./dataurlmaker.pl file1 file2 file3 ...\n\n";
-	exit(0);
+	main::HELP_MESSAGE();
+	exit;
 }
 
+# Iterate through files, print out data URL for each in turn
 foreach my $file(@ARGV)
 {
 	if (! -e $file)
