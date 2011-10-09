4f65788e333e51fb08e89c39a17f6c38512eee45
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2011-10-09 22:19:23 +0000

    Reorganization, module into own folder

--- build_packages.sh
+++ build_packages.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
 
+if [ ! -e command_line_tools/dataurlmaker.pl ]
+then
+    echo "must be run from Data URL Toolkit repository root"
+    exit
+fi
+
+# Remove old tarballs
+rm /tmp/DataURLToolkit.tgz &> /dev/null
+rm web/html/downloads/dataurlmaker.tgz &> /dev/null
+rm web/html/downloads/Apache-DataURL.tgz &> /dev/null
+
+# Create repository tarball
+tar cvfz /tmp/DataURLToolkit.tgz .
+
+# Move repo tarball to download path
+mv /tmp/DataURLToolkit.tgz web/html/downloads/DataURLToolkit.tgz
+
 # Perl command line tool
 tar cvfz web/html/downloads/dataurlmaker.tgz command_line_tools/dataurlmaker.pl
 
 # Apache module
-tar cvfz web/html/downloads/dataurlmaker.tgz command_line_tools/dataurlmaker.pl
+tar cvfz web/html/downloads/Apache-DataURL.tgz apache/Apache-DataURL.pm
