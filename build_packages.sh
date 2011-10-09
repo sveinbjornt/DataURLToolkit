8ddbdb7a4d05fd663b0dd167c1befbb355982e38
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2011-10-09 22:08:48 +0000

    Deprecating Mac OS X app

---
+++ build_packages.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Perl command line tool
+tar cvfz web/html/downloads/dataurlmaker.tgz command_line_tools/dataurlmaker.pl
+
+# Apache module
+tar cvfz web/html/downloads/dataurlmaker.tgz command_line_tools/dataurlmaker.pl
--- macosx/NSDataAdditions.h
+++
@@ -1,9 +0,0 @@
-
-#import <Cocoa/Cocoa.h>
-
-@interface NSData (NSDataAdditions)
-+ (NSData *) dataWithBase64EncodedString:(NSString *) string;
-- (id) initWithBase64EncodedString:(NSString *) string;
-- (NSString *) base64Encoding;
-- (NSString *) base64EncodingWithLineLength:(unsigned int) lineLength;
-@end
